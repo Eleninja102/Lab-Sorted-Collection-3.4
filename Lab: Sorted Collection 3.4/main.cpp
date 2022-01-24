31b6924764c2bc22da4862bc8b05b134b13b18e4
Coleton Watt <electricninja102@gmail.com>
2022-01-24 16:14:45 -0700

    Initial Commit

---
+++ Lab: Sorted Collection 3.4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab: Sorted Collection 3.4
+//
+//  Created by Coleton Watt on 1/24/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
